27bfccfbe998ffb13302bb9c4c1c4d492e26d87d
Pedro Paolo <pispedro97@gmail.com>
2023-10-16 14:30:04 -0300

    codigo atualizado para o Devel

--- TCC/myserver/src/app.js
+++ TCC/myserver/src/app.js
@@ -1,44 +1,37 @@
-// const express = require("express");
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-
-// app.listen(PORT, () => {
-//     console.log(`Server listening on ${PORT}`);
-// });
-
-// // Parte 1. Conexão com firewall, recuperação dinâmica dos dados provenientes da REST API Fortinet
-
-// app.get('/', (req, res) => {
-//     res.send('hello world');
-// });
-
 const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+let responseData; // Defina responseData fora do escopo do .then
 
-// Rota para fazer a requisição à URL e apresentar os dados no console
-app.get('/req', async (req, res) => {
-    try {
-        const response = await axios.get('https://10.69.70.254/api/v2/cmdb/firewall/address/?access_token=d4g1y41n66qgp1ymQsngbxckc7wyd3'); 
+// Realizando requisição ao FGT - Objeto bruto com todas as informações
 
+const url = 'https://fgt.nct.com.br/api/v2/monitor/wifi/unassociated-devices?with_triangulation=true&access_token=d4g1y41n66qgp1ymQsngbxckc7wyd3';
 
-        console.log('Dados da Requisição:');
-        console.log(response.data);
-        res.json(response.data); // Você também pode retornar os dados como resposta da rota
-         
-    } catch (error) {
-        console.error('Erro ao fazer a requisição:', error);
-        res.status(500).send('Erro ao fazer a requisição.');
+axios.get(url)
+    .then(response => {
+        responseData = response.data.results; // Atribua a responseData aqui
+
+        // Filtrar os resultados para objetos com um campo "triangulation_regions"
+        const objetosFiltrados = responseData.filter(objeto => objeto.triangulation_regions);
+
+        console.log('Objetos com triangulation_regions:');
+        console.log(objetosFiltrados);
+    })
+    .catch(error => {
+        console.error('Erro na requisição:', error);
+    });
+
+// Criação de método com framework express para exportar JSON recuperado
+
+app.get('/dados', (req, res) => {
+    if (responseData) {
+        res.json(responseData);
+    } else {
+        res.status(500).json({ error: 'Os dados não foram carregados ainda.' });
     }
 });
 
 app.listen(port, () => {
     console.log(`Servidor Express rodando na porta ${port}`);
 });
-
-
-
-
